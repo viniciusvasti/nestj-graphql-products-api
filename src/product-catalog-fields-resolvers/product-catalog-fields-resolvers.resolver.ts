50e27373bc187a175bc7d4972d2a6876afa5b995
Vinicius A. Santos <vinicius.vas.ti@gmail.com>
2024-05-05 12:01:13 -0300

    Refactor imports and remove unused services

--- src/product-catalog-fields-resolvers/product-catalog-fields-resolvers.resolver.ts
+++ src/product-catalog-fields-resolvers/product-catalog-fields-resolvers.resolver.ts
@@ -1,26 +1,20 @@
 import {
-  Args,
-  Context,
-  Int,
-  Parent,
-  Query,
-  ResolveField,
-  Resolver,
+    Args,
+    Context,
+    Int,
+    Parent,
+    Query,
+    ResolveField,
+    Resolver,
 } from '@nestjs/graphql';
-import { ProductCategoryService } from 'src/product-category/product-category.service';
-import { ProductDetailsService } from 'src/product-details/product-details.service';
-import { ProductInventoryService } from 'src/product-inventory/product-inventory.service';
-import { ProductPricesService } from 'src/product-price/product-price.service';
-import { ProductCatalogFieldResolver } from './dto/product-catalog-fields-resolvers.output';
 import { IDataLoader } from 'src/dataloader/dataloader.interface';
+import { ProductDetailsService } from 'src/product-details/product-details.service';
+import { ProductCatalogFieldResolver } from './dto/product-catalog-fields-resolvers.output';
 
 @Resolver(() => ProductCatalogFieldResolver)
 export class ProductCatalogFieldResolverResolver {
   constructor(
     private readonly productDetailsService: ProductDetailsService,
-    private readonly productPriceService: ProductPricesService,
-    private readonly productInventoryService: ProductInventoryService,
-    private readonly productCategoryService: ProductCategoryService,
   ) {}
 
   @Query(() => [ProductCatalogFieldResolver], {
